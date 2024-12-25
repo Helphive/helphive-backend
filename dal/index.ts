20f71f2a8a8a3bc077ec54e468bef9994698b49c
Maiz N. <contact.maiznadeem@gmail.com>
2024-12-25 16:21:11 +0500

    Changed node version

--- dal/index.ts
+++ dal/index.ts
@@ -3,3 +3,4 @@ export * from "./models/admin.model";
 export * from "./models/providerapplication.model";
 export * from "./models/booking.model";
 export * from "./models/payment.model";
+export * from "./models/earning.model";

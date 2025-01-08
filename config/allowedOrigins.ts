6c5b68dd9c207aa033f581eb46b0b46d1e76d39a
Maiz N. <contact.maiznadeem@gmail.com>
2025-01-09 02:06:42 +0500

    Fixed CORS Origins

--- config/allowedOrigins.ts
+++ config/allowedOrigins.ts
@@ -1,3 +1,12 @@
-const allowedOrigins = "*";
+const allowedOrigins = [
+	"http://localhost:5173",
+	"http://localhost:8080",
+	"http://localhost:3000",
+	"https://helphive--pr4-dev-cfzy9hyz.web.app/",
+	"https://helphive.web.app",
+	"https://helphive.firebaseapp.com",
+	"https://helphivenow.com",
+	"https://www.helphivenow.com",
+];
 
 export default allowedOrigins;
